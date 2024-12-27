6f68e274200a4b0c9073baaaf34e08bfd4742589
vincellob <vincellob@yahoo.com>
2024-12-26 20:27:16 -0500

    docker config for kafka

--- src/main/java/com/team2/backend/Kafka/Consumer/FavoriteGameConsumer.java
+++ src/main/java/com/team2/backend/Kafka/Consumer/FavoriteGameConsumer.java
@@ -3,35 +3,35 @@ package com.team2.backend.Kafka.Consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.team2.backend.Model.Game;
-import com.team2.backend.Service.GameService;  
+// import com.team2.backend.Model.Game;
+// import com.team2.backend.Service.GameService;  
 public class FavoriteGameConsumer {
 
-    @Autowired
-    private GameService gameService;
+    // @Autowired
+    // private GameService gameService;
 
-    @KafkaListener(topics = "favorited_games", groupId = "game-forum-group")
-    public void listenFavoritedGame(String message) {
-        System.out.println("Received favorited game message: " + message);
+    // @KafkaListener(topics = "favorited_games", groupId = "game-forum-group")
+    // public void listenFavoritedGame(String message) {
+    //     System.out.println("Received favorited game message: " + message);
         
-        Game game = deserializeGame(message);
+    //     Game game = deserializeGame(message);
 
-        if (game != null) {
-            gameService.addFavorite(game);
-            System.out.println("Game favorited: " + game.getTitle());
-        } else {
-            System.err.println("Failed to deserialize the game message.");
-        }
-    }
+    //     if (game != null) {
+    //         gameService.addFavorite(game);
+    //         System.out.println("Game favorited: " + game.getTitle());
+    //     } else {
+    //         System.err.println("Failed to deserialize the game message.");
+    //     }
+    // }
 
   
-    private Game deserializeGame(String message) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readValue(message, Game.class);
-        } catch (Exception e) {
-            System.err.println("Error deserializing game message: " + e.getMessage());
-            return null;
-        }
-    }
+    // private Game deserializeGame(String message) {
+    //     try {
+    //         ObjectMapper objectMapper = new ObjectMapper();
+    //         return objectMapper.readValue(message, Game.class);
+    //     } catch (Exception e) {
+    //         System.err.println("Error deserializing game message: " + e.getMessage());
+    //         return null;
+    //     }
+    // }
 }
