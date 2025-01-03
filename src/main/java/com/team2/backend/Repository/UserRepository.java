6625e4a1627dce0b8d1ff60d9b656b749cc85db4
jbseale <jbseale@buffalo.edu>
2025-01-03 14:04:19 -0500

    Restore Repository folder

--- src/main/java/com/team2/backend/Repository/UserRepository.java
+++ src/main/java/com/team2/backend/Repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.team2.backend.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.team2.backend.Models.User;
 
 public interface UserRepository extends JpaRepository<User,Long>{
+
+    Optional<User> findByUsername(String string);
     
 }
