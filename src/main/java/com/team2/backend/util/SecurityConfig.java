20eee6dcf6bcf82acddc352f38d6cd982381a90f
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2025-01-12 21:41:52 -0500

    updated allowed origins to include ec2 frontend instance private ip

--- src/main/java/com/team2/backend/util/SecurityConfig.java
+++ src/main/java/com/team2/backend/util/SecurityConfig.java
@@ -31,7 +31,10 @@ public class SecurityConfig {
                 .cors(cors -> {
                     CorsConfiguration configuration = new CorsConfiguration();
                     // If you need credentials (cookies), do NOT use "*"
-                    configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+                    configuration.setAllowedOrigins(List.of(
+                            "http://localhost:8080", // Testing
+                            "http://172.31.1.83" // Private ip of frontend ec2 instance
+                    ));
                     // Or if no credentials needed, you can do "*"
                     // configuration.setAllowedOrigins(List.of("*"));
 
